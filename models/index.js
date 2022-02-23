4a02839d1e71e7256df9359c624590d8fc1c92f3
Maribel Montes <maribel.doris@gmail.com>
2022-02-22 22:47:59 -0600

    added the index module

---
+++ models/index.js
@@ -0,0 +1,42 @@
+const Comment = require('./Comment');
+const Post = require('./Post');
+const User = require('./User');
+
+/** Model Relationship
+ * user has many post *
+ * user can have many coments *
+ * coments belong to one post *
+ * post can have many comments *
+ * post belong to one user *
+ */
+
+//user has many post
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+//user can have many coments
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+});
+
+//coments belong to one post
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+//post can have many comments
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE',
+});
+
+//post belong to one user 
+Post.belongsTo(User, {
+    foreignKet: 'user_id'
+});
+
+
+module.exports = { Comment, Post, User };
