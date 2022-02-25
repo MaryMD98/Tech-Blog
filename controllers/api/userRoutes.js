8d8789ee14e85f2ae47ecd6d51cb6f77e6125791
Maribel Montes <maribel.doris@gmail.com>
2022-02-25 11:27:50 -0600

    added the login route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -8,7 +8,17 @@ router.post();
 // login
 router.post('/login', async (req,res) => {
     try{
-        const userData = await User.findOne();
+        const userData = await User.findOne({where: { email: req.body.email }});
+        if(!userData){res.status(400).json({message: 'Incorrect email or password, please try again'}); return;}
+
+        const ValidPassword = await userData.checkPassword(req.body.password);
+        if(!ValidPassword){res.status(400).json({message: 'Incorrect email or password, please try again'}); return;}
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+            res.json({user: userData, message: 'You are now Logged in!'});
+        });
     }
     catch(err){ res.status.json(err);}
 });
