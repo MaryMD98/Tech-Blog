d5438a15096f144e359586668e87b1719b7fd798
Maribel Montes <maribel.doris@gmail.com>
2022-02-27 23:55:25 -0600

    aded the route to get posts by user

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,6 +2,21 @@ const router = require('express').Router();
 const { Comment, Post, User } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+// Display the posts created by a user
+router.get('/', withAuth, async (req, res) => {
+    try{
+        const UserData = await User.findByPk(req.session.user_id, {
+            attributes: { excluse: ['password'] },
+            include: [ {model: Post, attributes:['id', 'title','date'],} ]
+        });
+        if(!UserData){ res.status(400).json({message: 'No user found with that ID!'}); return; }
+        const userPosts = UserData.get({ plain: true });
+        res.status(200).json(userPosts);
+        // res.render("somdgerget", {userPosts, logged_in: req.session.logged_in });
+    }
+    catch(err){ res.status(500).json(err);}
+});
+
 // create a user
 router.post('/', async (req, res) => {
     try{
