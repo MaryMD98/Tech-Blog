0bcc366800873b401ba3c8da7d1b949c70bc0874
Maribel Montes <maribel.doris@gmail.com>
2022-02-27 23:37:51 -0600

    added the create login route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -3,7 +3,21 @@ const { Comment, Post, User } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 // create a user
-router.post();
+router.post('/', async (req, res) => {
+    try{
+        const userData = await User.create({
+            username: req.body.username,
+            password: req.body.passowrd,
+        });
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+            res.status(200).json(userData);
+        });
+    }
+    catch(err){ res.status(500).json(err); }
+});
 
 // login
 router.post('/login', async (req,res) => {
