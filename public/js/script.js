b8195b4690c9f3acad30412c94a2a5f1264b4d20
Maribel Montes <maribel.doris@gmail.com>
2022-03-01 22:10:05 -0600

    added event listener for creating a new post

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,38 @@
+// handler for creating a new post
+const newPOST = async (event) => {
+    event.preventDefault();
+    // get the information from the form
+    const title = document.querySelector('#postTitle').value.trim();
+    const content = document.querySelector('#postContent').value.trim();
+
+    if(title && content){
+        const response = await fetch(`/api/posts/`, {
+            method: 'POST',
+            body: JSON.stringify({title, content}),
+            headers: {'Content-Type': 'application/json'},
+        });
+        if(response.ok){ document.location.replace('/api/users/'); }
+        else {alert('failed to create a post');}
+    }
+};
+
+// handler for creating a new comment
+
+// handler for updating a post
+
+// handler for deleting a post
 
 // event listener for creating a new post
-document.querySelector('#createPOST');
+document.querySelector('#createPOST').addEventListener('submit', newPOST);
+
 // event listener for adding a new comment
-document.querySelector('#comButton');
+document.querySelector('#comentNew');
 
 // event listener to update a post
+document.querySelector('#updatePost');
+
 // event listener to delete a post
+document.querySelector('#deletePost');
 
 
 /**pseudo-code hw14-MVC - blog site
