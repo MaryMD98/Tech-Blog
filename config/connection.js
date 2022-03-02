4a82466477a5360721890f893408fa0b9e3c762f
Maribel Montes <maribel.doris@gmail.com>
2022-03-02 02:53:10 -0600

    added the jawsdb and screenshots for the readme

--- config/connection.js
+++ config/connection.js
@@ -3,6 +3,9 @@ const Sequelize = require('sequelize');
 
 let sequelize;
 
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+  } else {
     sequelize = new Sequelize(
         process.env.DB_NAME,
         process.env.DB_USER,
@@ -13,5 +16,6 @@ let sequelize;
             port: 3306
         }
     );
+}
 
 module.exports = sequelize;
