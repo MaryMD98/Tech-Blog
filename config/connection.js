6d7914487e81db546c1f1acb8e1d039bd9efdcb8
Maribel Montes <maribel.doris@gmail.com>
2022-03-08 03:11:44 -0600

    testing the connection

--- config/connection.js
+++ config/connection.js
@@ -3,25 +3,20 @@ const Sequelize = require('sequelize');
 
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL)//, {
-    //     host: 'l6glqt8gsx37y4hs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    //     // dialect: 'mysql',
-    //     // dialectOptions: {
-    //     //     decimalNumbers: true,
-    //     // }
-    // });
-  } else {
-    sequelize = new Sequelize(
-        process.env.DB_NAME,
-        process.env.DB_USER,
-        process.env.DB_PASSWORD,
-        {
-            host: 'localhost',
-            dialect: 'mysql',
-            port: 3306
-        }
-    );
-}
+// if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+    
+//   } else {
+    // sequelize = new Sequelize(
+    //     process.env.DB_NAME,
+    //     process.env.DB_USER,
+    //     process.env.DB_PASSWORD,
+    //     {
+    //         host: 'localhost',
+    //         dialect: 'mysql',
+    //         port: 3306
+    //     }
+    // );
+// }
 
 module.exports = sequelize;
